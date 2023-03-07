3543a81563cca6d7a75fdb285bc1dd0fef9c09d5
Welley <monteirowelley7@gmail.com>
2023-03-07 15:36:51 -0300

    config admin

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import GalleryArt
 
 class ListGalleryArt(admin.ModelAdmin):
-    list_display = ('title', 'date_create')
+    list_display = ('id','title', 'date_create')
+    list_display_links = ('id','title', 'date_create',)
+    list_per_page = 10
 
 admin.site.register(GalleryArt, ListGalleryArt)
